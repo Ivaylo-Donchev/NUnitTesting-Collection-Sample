ba0d808e7742c6c6b34a1f61f3bf7d1fb6f39c9e
IvailitoMan <ivailitoman@gmail.com>
2023-02-05 09:58:28 +0200

    new Collection Nunit Tests

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -1,3 +1,5 @@
+using Collections;
+
 namespace Collection.UnitTests
 {
     public class CollectionTests
@@ -5,9 +7,83 @@ namespace Collection.UnitTests
 
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public void Test_Collection_EmptyConstructor() {  
+
+            //ARRANGE and ACT
+            var coll = new Collection<int>();
+
+            //ASSERT
+            Assert.AreEqual(coll.ToString(), "[]");           
+
         }
+
+        [Test]
+        public void Test_Collection_ConstructorSingleItem() 
+        {
+        var coll = new Collection<int>(5);
+            Assert.AreEqual(coll.ToString(), "[5]");
+        }
+
+        [Test]
+        public void Test_Collection_ConstructorMultipleItems() 
+        {
+            var coll = new Collection<int>(5, 6);
+            Assert.AreEqual(coll.ToString(), "[5, 6]");
+        }
+
+        [Test]
+        public void Test_Collection_CountAndCapacity() 
+        {
+            var coll = new Collection<int>(5, 6);
+
+            //ASSERT
+            Assert.AreEqual(coll.Count, 2);
+            Assert.That(coll.Capacity, Is.GreaterThan(coll.Count));
+        }
+
+
+        [Test]
+        public void Test_Collection_Add()
+        {
+            //ARRANGE
+            
+            var coll = new Collection<string>("Ivan", "Georgi");
+
+            //ACT
+            
+            coll.Add("Stefan");
+
+            //ASSERT
+
+            Assert.AreEqual(coll.ToString(), "[Ivan, Georgi, Stefan]");
+
+        }
+
+        [Test]
+        public void Test_GetByInvalidIndex() 
+        {
+            var coll = new Collection<int>(5, 6, 7);
+            var item = coll[1];
+
+            Assert.That(item.ToString(), Is.EqualTo("6"));
+        }
+
+        [Test]
+        public void Test_Collection_GetByIndex()
+        {
+            var coll = new Collection<int>(5, 6, 7);
+            coll[1] = 666;
+
+            Assert.That(coll.ToString(), Is.EqualTo("[5, 666, 7]"));
+        }
+
+        [Test]
+        public void Test_Collection_GetByInvalidIndex() 
+        {
+        //ARRANGE
+        var coll = new Collection<string>("Ivan", "Stefan");
+            Assert.That(() => { var item = coll[2]; }, Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+
     }
 }
